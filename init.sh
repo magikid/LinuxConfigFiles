dda4d0ff439fe4333be5d5c7d65513111cc034a7
Chris W Jones <chris@christopherjones.us>
2015-05-31 14:01:42 -0400

    Updated init script
    Now deletes the .vim folder and then symlinks it to the one in the repo.

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Should be run from ~
 
+rm -rf ~/.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 rm .vimrc .gitconfig .bashrc .bash_alias .tmux.conf
+ln -s .dotfiles/vim .vim
 ln -s .dotfiles/vimrc .vimrc
 ln -s .dotfiles/gitconfig .gitconfig
 ln -s .dotfiles/bashrc .bashrc
